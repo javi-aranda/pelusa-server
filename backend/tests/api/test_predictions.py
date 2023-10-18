a73b7dcf971296081fb8a18748b456693cda6eea
Javier Aranda Santos <javier.aranda.designs@gmail.com>
2023-10-18 12:22:46 +0200

    Use mock in predictions API

--- backend/tests/api/test_predictions.py
+++ backend/tests/api/test_predictions.py
@@ -1,13 +1,18 @@
 from typing import Callable
 
+import numpy as np
 from httpx import AsyncClient
+from pytest_mock import MockerFixture
 
 from app.core.config import settings
 
 
-async def test_analysis(client: AsyncClient) -> None:
+async def test_analysis(client: AsyncClient, mocker: MockerFixture) -> None:
+    # Assume that the model will always predict 0 (legitimate)
+    mocker.patch("app.api.predictions.analyze_url", return_value=(np.zeros(5), 0))
+
     resp = await client.post(
-        f"{settings.API_PATH}/analysis", json={"input": "https://example.com"}
+        f"{settings.API_PATH}/analysis", json={"input": "https://example.es"}
     )
     data = resp.json()
     assert resp.status_code == 201
@@ -15,8 +20,11 @@ async def test_analysis(client: AsyncClient) -> None:
 
 
 async def test_analysis_stored_in_db(
-    client: AsyncClient, create_prediction: Callable
+    client: AsyncClient, create_prediction: Callable, mocker: MockerFixture
 ) -> None:
+    # Assume that the model will always predict 0 (legitimate) but it's already stored as 1 (phishing)
+    mocker.patch("app.api.predictions.analyze_url", return_value=(np.zeros(5), 0))
+
     prediction = await create_prediction("https://example.org", 1)
 
     resp = await client.post(
@@ -28,7 +36,11 @@ async def test_analysis_stored_in_db(
     assert prediction == 1
 
 
-async def test_analysis_force(client: AsyncClient, create_prediction: Callable) -> None:
+async def test_analysis_force(
+    client: AsyncClient, create_prediction: Callable, mocker: MockerFixture
+) -> None:
+    # Assume that the model will always predict 0 (legitimate) but it's already stored as 1 (phishing), but we force the analysis
+    mocker.patch("app.api.predictions.analyze_url", return_value=(np.zeros(5), 0))
     prediction = await create_prediction("https://example.net", 1)
 
     resp = await client.post(
